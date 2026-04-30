d91c5e9db874e0c4abfb17f4cf9bafa45829e36e
Sani <saniyousifauni@gmail.com>
2026-04-29 22:48:54 -0500

    auth redirect fix

--- components/AuthStateContext.jsx
+++ components/AuthStateContext.jsx
@@ -60,7 +60,7 @@ export function AuthStateProvider({ children }) {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'github',
                 options: {
-                    redirectTo: `${window.location.origin}/auth/callback`,
+                    redirectTo: `${window.location.origin}/p169/auth/callback`,
                 },
             })
             return { error }
